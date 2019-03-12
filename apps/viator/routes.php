081c6a8d7b4986c223395e294d7156b3dc636c4e
mangotonk <scott.maclaren@junction6travel.com>
2019-03-13 12:20:25 +1300

    ugly but decentralised routes definition

---
+++ apps/viator/routes.php
@@ -0,0 +1,41 @@
+<?php
+
+use Phalcon\Mvc\Router\Group as RouterGroup;
+
+// Create a group with a common module and controller
+$blog = new RouterGroup(
+    [
+        'module'     => 'viator',
+        'controller' => 'index',
+    ]
+);
+
+// All the routes start with /blog
+$blog->setPrefix('/blog');
+
+// Add a route to the group
+//$blog->add(
+//    '/save',
+//    [
+//        'action' => 'save',
+//    ]
+//);
+//
+//// Add another route to the group
+$blog->add(
+    '/edit/{id}',
+    [
+        'action' => 'edit',
+    ]
+);
+
+// This route maps to a controller different than the default
+$blog->add(
+    '/glob',
+    [
+        'controller' => 'index',
+        'action'     => 'steve',
+    ]
+);
+
+return $blog;
--- config/routes.php
+++ config/routes.php
@@ -1,48 +1,10 @@
 <?php
 
 use Phalcon\Mvc\Router;
-use Phalcon\Mvc\Router\Group as RouterGroup;
 
 $router = new Router();
 
-// Create a group with a common module and controller
-$blog = new RouterGroup(
-    [
-        'module'     => 'viator',
-        'controller' => 'index',
-    ]
-);
-
-// All the routes start with /blog
-$blog->setPrefix('/blog');
-
-// Add a route to the group
-//$blog->add(
-//    '/save',
-//    [
-//        'action' => 'save',
-//    ]
-//);
-//
-//// Add another route to the group
-$blog->add(
-    '/edit/{id}',
-    [
-        'action' => 'edit',
-    ]
-);
-
-// This route maps to a controller different than the default
-$blog->add(
-    '/glob',
-    [
-        'controller' => 'index',
-        'action'     => 'steve',
-    ]
-);
-
-// Add the group to the router
-$router->mount($blog);
-
-return $router;
-
+function bob ($router){
+	$router->mount(require_once '../apps/viator/routes.php');
+}
+bob($router);
